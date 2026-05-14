7436993337b387eeacc07bb294d1d1d2a5356e7f
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-05-13 23:46:04 -0400

    Update online.php

--- online.php
+++ online.php
@@ -1,1 +1,20 @@
+<?php
 
+$url = "http://127.0.0.1/panel/api.php";
+
+$data = [
+    'token' => '123456',
+    'action' => 'online'
+];
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/x-www-form-urlencoded",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ],
+];
+
+$result = file_get_contents($url, false, stream_context_create($options));
+
+echo $result;
