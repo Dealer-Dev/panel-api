3658ff02aa05b6d393f42ce79a8fd100b8d26f79
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-05-19 01:38:46 -0400

    Update online.php

--- online.php
+++ online.php
@@ -1,20 +1,38 @@
 <?php
 
+$configFile = "/etc/panel-api.conf";
+
+$config = parse_ini_file($configFile);
+
+$TOKEN = $config['TOKEN'] ?? '123456';
+
 $url = "http://127.0.0.1/panel/api.php";
 
 $data = [
-    'token' => '123456',
+
+    'token' => $TOKEN,
+
     'action' => 'online'
 ];
 
 $options = [
+
     'http' => [
-        'header'  => "Content-type: application/x-www-form-urlencoded",
-        'method'  => 'POST',
-        'content' => http_build_query($data),
+
+        'header' =>
+        "Content-type: application/x-www-form-urlencoded",
+
+        'method' => 'POST',
+
+        'content' =>
+        http_build_query($data),
     ],
 ];
 
-$result = file_get_contents($url, false, stream_context_create($options));
+$result = file_get_contents(
+    $url,
+    false,
+    stream_context_create($options)
+);
 
 echo $result;
